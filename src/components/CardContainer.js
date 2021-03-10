eeb53a1d74607f27f1a96912cdc908cc3e7c4d8d
wkoslik <wkoslik@gmail.com>
2021-03-10 11:43:56 -0500

    nrandom number passing to cards component and rendering

--- src/components/CardContainer.js
+++ src/components/CardContainer.js
@@ -3,10 +3,14 @@ import React, { useState } from 'react'
 import Cards from './Cards'
 
 const CardContainer  = (props) => {
+    let allCards = props.cardNumbers.map(num =>{
+        return <Cards number={num} />
+    })
+
     return (
         <div className="card-container">
             <h1>I will hold the cards</h1>
-            <Cards />
+            {allCards}
         </div>
     )
 }
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,11 +1,12 @@
 //individual cards
 //Each card has a set pixel size, such as 300px by 250px, but you can decide what size you wish to use. Upon instantiation, a card should be given a random number between 0 and 100 and display it in the center of the card.
 //Each card has a button in the top-right corner which can be used to delete the card.
+import React from 'react'
 
 const Cards = (props) => {
     return (
         <div>
-            <h1> I will be a card </h1>
+            <h1>{props.number}</h1>
         </div>
     )
 }
